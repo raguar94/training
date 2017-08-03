f9afee3999f16bec048a2ccaa3d7dc5e53f83a35
Mateo Noreña <npmatt23@gmail.com>
2017-08-03 08:45:07 -0500

    Updated mattnp.scala

--- challenges/codeabbey/006/mattnp.scala
+++ challenges/codeabbey/006/mattnp.scala
@@ -1,9 +1,8 @@
 object Solution extends App {
-  def roundUp(d: Double) = math.round(d).toInt
   def div(l: Array[Double]) =  l(0)/l(1)
   val n = scala.io.StdIn.readInt()
   for (_ <- 1 to n) {
-    val res = roundUp(div(scala.io.StdIn.readLine.split(" ").map(_.toDouble)))
+    val res = div(scala.io.StdIn.readLine.split(" ").map(_.toDouble)).round
     System.out.println(res + " ")
   }
 }
