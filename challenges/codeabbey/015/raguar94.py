3dc802b28f2525a7f1fa0d4cc727255051e4474c
Arnold <arnoldgr6@gmail.com>
2017-08-13 13:24:24 -0500

    Create raguar94.py

---
+++ challenges/codeabbey/015/raguar94.py
@@ -0,0 +1,17 @@
+x=raw_input().split()
+l=len(x)
+for i in range(l):
+    if i==0:
+        max=int(x[i])
+    if i==1:
+        if max>int(x[i]):
+            min=int(x[i])
+        else:
+            min=max
+            max=int(x[i])
+    if i>1:
+        if int(x[i])>max:
+            max=int(x[i])
+        if int(x[i])<min:
+            min=int(x[i])
+print str(max)+' '+str(min)
