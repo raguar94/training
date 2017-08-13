dda05ba521798ba6be32c4f627435e7161e41c8f
Arnold <arnoldgr6@gmail.com>
2017-08-13 13:25:26 -0500

    Create raguar94.py

---
+++ challenges/codeabbey/016/raguar94.py
@@ -0,0 +1,9 @@
+x=int(raw_input())
+for i in range(x):
+    y=raw_input().split()
+    l=len(y)-1
+    suma=0
+    for j in range(l):
+        suma+=float(y[j])
+    avg=suma/l
+    print str(int(round(avg)))+' '
