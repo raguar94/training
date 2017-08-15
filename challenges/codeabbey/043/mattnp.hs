a51181b2637266a76e9ce45f4e1637f4ca07b6f5
Mateo Noreña <npmatt23@gmail.com>
2017-08-15 01:42:40 -0500

    Solved Dice Rolling (043) from CodeAbbey.

---
+++ challenges/codeabbey/043/mattnp.hs
@@ -0,0 +1,16 @@
+import Data.List
+
+main = do
+  n <- readLn :: IO Int
+  readRandoms n
+
+readRandoms :: Int -> IO ()
+readRandoms 0 = return ()
+readRandoms n = do
+  random <- readLn :: IO Float
+  putStr $ show $ diceRolling random
+  putStr " "
+  readRandoms (n-1)
+
+diceRolling :: Float -> Int
+diceRolling x = (floor (x*6)) + 1
