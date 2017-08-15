a7dc4625ede543bce7c757dfca29e013d2bb8f7e
Mateo Noreña <npmatt23@gmail.com>
2017-08-15 00:55:33 -0500

    Solved Median of Three (041) from CodeAbbey.

---
+++ challenges/codeabbey/041/mattnp.hs
@@ -0,0 +1,18 @@
+import Data.List
+
+main = do
+  n <- readLn :: IO Int
+  readArrays n
+
+readArrays :: Int -> IO ()
+readArrays 0 = return ()
+readArrays n = do
+  array <- getLine
+  let xs = map read $ words array :: [Int]
+  putStr $ show $ medianOfThree xs
+  putStr " "
+  readArrays (n-1)
+
+medianOfThree :: [Int] -> Int
+medianOfThree xs = do
+  sort xs !! 1
